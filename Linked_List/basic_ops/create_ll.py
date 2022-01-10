85e88b3577e2d14e70d08a8efd00ac2a4ef7a08c
Ruth Obe <ruthobey@gmail.com>
2022-01-10 00:29:50 -0600

    create method to print nodes in linked list

--- Linked_List/basic_ops/create_ll.py
+++ Linked_List/basic_ops/create_ll.py
@@ -8,7 +8,7 @@ class LinkedList:
     def get_head_node(self):
         return self.head_node
 
-    # add node to beginning of ll
+    # add node to beginning of ll - prepend
     def add_to_beginning(self, value):
         if self.head_node.get_value() == None:
             self.head_node.set_value(value)
@@ -16,3 +16,14 @@ class LinkedList:
             new_node = Node(value)
             new_node.set_next_node(self.head_node)
             self.head_node = new_node
+
+    # print ll
+    def printll(self):
+        if self.head_node.get_value() == None:
+            print("Linked list is empty!")
+        else:
+            current_node = self.head_node
+            while current_node:
+                if current_node.get_value() != None:
+                    print(current_node.get_value(), end=" ")
+                current_node = current_node.get_next_node()
--- Linked_List/basic_ops/main.py
+++ Linked_List/basic_ops/main.py
@@ -1,9 +1,12 @@
 from create_ll import LinkedList 
 
-ll = LinkedList()
+ll = LinkedList(23)
 print(ll.get_head_node().get_value())
 
 ll.add_to_beginning(35)
 
 # prints value of new head node --> should print 35
 print(ll.get_head_node().get_value())
+
+# print ll --> should ouptput 35 23
+ll.printll()
